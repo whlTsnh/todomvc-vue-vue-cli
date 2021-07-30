794da2cbacd58594a4f39744532360a8be68ae58
whl1016 <wanghl_1016@126.com>
2021-07-30 18:06:26 +0800

    用于了实验

--- src/components/completed.vue
+++ src/components/completed.vue
@@ -1,43 +1,51 @@
-
 <template>
-    <div>
-		<!-- 向子组件传值 -->
-        <info :todos="$store.state.todos.filter(todo=>todo.completed)"></info>
-			<!-- This footer should hidden by default and shown when there are todos -->
-			<footer class="footer">
-				<!-- This should be `0 items left` by default -->
-				<span class="todo-count"><strong>{{$store.state.todos.filter(todo=>todo.completed).length}}</strong> item left</span>
-				<!-- Remove this if you don't implement routing -->
-				<ul class="filters">
-					<li>
-            <router-link to="/all">All</router-link>
-					</li>
-					<li>
-						<router-link to="/active">Active</router-link>
-					</li>
-					<li>
-						<router-link class="selected" to="/completed">Completed</router-link>
-					</li>
-				</ul>
-				<!-- Hidden if no completed items are left ↓ -->
-				<button class="clear-completed" @click="deleteAllCom" v-show="$store.getters.s">Clear completed</button>
-			</footer>
-    </div>
+  <div>
+    <!-- 向子组件传值 -->
+    <info :todos="$store.state.todos.filter((todo) => todo.completed)"></info>
+    <!-- This footer should hidden by default and shown when there are todos -->
+    <footer class="footer">
+      <!-- This should be `0 items left` by default -->
+      <span class="todo-count"
+        ><strong>{{
+          $store.state.todos.filter((todo) => todo.completed).length
+        }}</strong>
+        item left</span
+      >
+      <!-- Remove this if you don't implement routing -->
+      <ul class="filters">
+        <li>
+          <router-link to="/all">All</router-link>
+        </li>
+        <li>
+          <router-link to="/active">Active</router-link>
+        </li>
+        <li>
+          <router-link class="selected" to="/completed">Completed</router-link>
+        </li>
+      </ul>
+      <!-- Hidden if no completed items are left ↓ -->
+      <button
+        class="clear-completed"
+        @click="deleteAllCom"
+        v-show="$store.getters.s"
+      >
+        Clear completed
+      </button>
+    </footer>
+  </div>
 </template>
 
 <script>
-import info from './todos.vue'
+import info from "./todos.vue";
 export default {
-    components:{
-        info
+  components: {
+    info,
+  },
+  methods: {
+    deleteAllCom() {
+      this.$store.commit("deleteAllCom");
     },
-    methods:{
-		deleteAllCom(){
-			this.$store.commit('deleteAllCom')
-		}
-    }
-}
+  },
+};
 </script>
-<style lang="scss" scoped>
-    
-</style>
+<style lang="scss" scoped></style>
